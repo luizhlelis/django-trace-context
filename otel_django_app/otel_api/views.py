e271e07fe64ac67ca65a2c585769055fca39daee
Luiz Lelis <luizhlelis@gmail.com>
2021-07-28 23:47:46 -0300

    chore: exporter update to `zipkin`

--- otel_django_app/otel_api/views.py
+++ otel_django_app/otel_api/views.py
@@ -1,21 +1,20 @@
 from django.http import HttpResponse
 from opentelemetry import trace
-from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.exporter.zipkin.json import ZipkinExporter
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
-trace.set_tracer_provider(TracerProvider())
+trace.set_tracer_provider(TracerProvider(resource=Resource.create({SERVICE_NAME: "django-server"})))
 tracer = trace.get_tracer(__name__)
 
-# create a JaegerExporter
-jaeger_exporter = JaegerExporter(
-    # configure agent
-    agent_host_name='localhost',
-    agent_port=9411
+# create a ZipkinExporter
+zipkin_exporter = ZipkinExporter(
+    endpoint="http://localhost:9411/api/v2/spans",
 )
 
 # Create a BatchSpanProcessor and add the exporter to it
-span_processor = BatchSpanProcessor(jaeger_exporter)
+span_processor = BatchSpanProcessor(zipkin_exporter)
 
 # add to the tracer
 trace.get_tracer_provider().add_span_processor(span_processor)
